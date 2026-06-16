f79380cbac0a35cd1b36457449be74de0bba2b09
Lucas de Oliveira Mota <lucas.d.mota@ufv.br>
2026-06-16 17:47:54 -0300

    Adiciona suporte para configuração de URL da API no app.json e ajusta a baseURL no serviço de API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,23 @@
 import axios from 'axios';
+import Constants from 'expo-constants';
+
+function getDefaultBaseURL() {
+    const debuggerHost = Constants.manifest && Constants.manifest.debuggerHost;
+    const host = debuggerHost && debuggerHost.split(':')[0];
+
+    if (host) {
+        return `http://${host}:3333`;
+    }
+
+    return 'http://localhost:3333';
+}
+
+const configuredBaseURL = Constants.manifest
+    && Constants.manifest.extra
+    && Constants.manifest.extra.apiUrl;
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.14:3333'
+    baseURL: configuredBaseURL || getDefaultBaseURL(),
 });
 
 export default api;
